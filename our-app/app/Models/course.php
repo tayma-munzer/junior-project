cca7015e3efdc7bcba14946e3990441d077daf52
ghina_alqaseer <ghinaalqaseer@gmail.com>
2024-05-09 14:27:50 +0200

    resolved merge conflicts

--- our-app/app/Models/course.php
+++ our-app/app/Models/course.php
@@ -16,5 +16,13 @@ class course extends Model
         'c_price',
         'c_img',
         'u_id',
+        'c_duration',
+        'pre_requisite',
+        'ct_id',
     ];
+
+    public function courses_type(){
+        return 
+        $this->belongsTo(courses_type::class,'ct_id','c_id');
+    }
 }
--- our-app/database/migrations/2024_04_05_113824_create_courses_table.php
+++ our-app/database/migrations/2024_04_05_113824_create_courses_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -18,7 +20,10 @@ return new class extends Migration
             $table->integer('c_price');
             $table->mediumText('c_img');
             $table->integer('u_id');
-          
+            $table->string('c_duration');
+            $table->string('pre_requisite');
+
+        
           
 
         
