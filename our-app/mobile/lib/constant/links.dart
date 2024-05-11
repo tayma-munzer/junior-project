b9f9b984d7f525f42b940194828360e6c1354b2a
ghina_alqaseer <ghinaalqaseer@gmail.com>
2024-05-11 15:52:24 +0200

    resolved merge conflicts

--- our-app/mobile/lib/constant/links.dart
+++ our-app/mobile/lib/constant/links.dart
@@ -79,3 +79,4 @@ const get_exp = URL + "get_exp";
 const get_courses_type = URL + "get_courses_type";
 const get_course_details = URL + 'get_course_details';
 const get_courses_for_type = URL + 'get_courses_for_type';
+const get_course_for_user = URL + 'get_course_for_user';
