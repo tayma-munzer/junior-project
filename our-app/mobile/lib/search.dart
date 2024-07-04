ed6b74599e3bfd41a92d1bc1993ca8fd5771afea
tayma-munzer <164791076+tayma-munzer@users.noreply.github.com>
2024-07-04 18:43:24 +0300

    search front end

--- our-app/mobile/lib/search.dart
+++ our-app/mobile/lib/search.dart
@@ -1,13 +1,17 @@
-// ignore_for_file: unused_import
 import 'dart:convert';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:mobile/CategoriesDetails.dart';
 import 'package:mobile/appbar.dart';
 import 'package:mobile/bottombar.dart';
 import 'package:mobile/drawer.dart';
-import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:mobile/readcourse.dart';
+import 'package:mobile/viewcoursetobuy.dart';
+import 'package:mobile/viewjob.dart';
+import 'package:mobile/viewservice.dart';
+import 'package:mobile/viewservicetobuy.dart';
+import 'package:mobile/wallet.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -18,19 +22,18 @@ class SearchPage extends StatefulWidget {
 
 class _SearchPageState extends State<SearchPage> {
   TextEditingController _controller = TextEditingController();
-  var socket;
+  late WebSocket socket;
   String _selectedCategory = 'services';
   final List<String> _categories = ['services', 'courses', 'jobs'];
   List<dynamic>? _searchResults;
   Map<String, dynamic>? results;
-  List? indexes;
+  List<String>? indexes;
 
   Future<void> connect(url) async {
     try {
       print("try");
       socket = await WebSocket.connect(url);
       print('WebSocket connected');
-      //final data = {'search_string': _controller.text};
       final data = {'search_string': 'قواعد'};
       socket.add(jsonEncode(data));
       socket.listen(
@@ -68,25 +71,41 @@ class _SearchPageState extends State<SearchPage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
+            Text(
+              'عن ماذا تريد ان تبحث',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
             Row(
               children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      labelText: 'عن ماذا تريد البحث',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
                 SizedBox(width: 10),
                 ElevatedButton(
                   onPressed: () {
                     connect("ws://10.0.2.2:8767");
                   },
-                  child: Text('بحث '),
+                  child: Text(
+                    'بحث',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        Color.fromARGB(255, 243, 176, 145)),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    textAlign: TextAlign.right,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    cursorColor: Colors.black,
+                  ),
                 ),
               ],
             ),
@@ -104,8 +123,7 @@ class _SearchPageState extends State<SearchPage> {
                         setState(() {
                           _selectedCategory = category;
                           _searchResults = results![_selectedCategory];
-                          Map<String, dynamic> data = _searchResults![0];
-                          indexes = data.keys.toList();
+                          indexes = _searchResults![0].keys.toList();
                           print(_searchResults);
                         });
                       },
@@ -117,13 +135,37 @@ class _SearchPageState extends State<SearchPage> {
             SizedBox(height: 20),
             if (indexes != null && results != null)
               Expanded(
-                child: ListView.builder(
-                  itemCount: _searchResults!.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      title: Text(_searchResults![index][indexes![1]]),
-                    );
-                  },
+                child: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: ListView.builder(
+                    itemCount: _searchResults!.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: InkWell(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  if (_selectedCategory == 'services') {
+                                    return CategoriesDetails(
+                                        _searchResults![index][indexes![0]]);
+                                  } else if (_selectedCategory == 'courses') {
+                                    return CourseDetailsPage(
+                                        _searchResults![index][indexes![0]]);
+                                  } else {
+                                    return viewjob(
+                                        _searchResults![index][indexes![0]]);
+                                  }
+                                },
+                              ),
+                            );
+                          },
+                          child: Text(_searchResults![index][indexes![1]]),
+                        ),
+                      );
+                    },
+                  ),
                 ),
               ),
           ],
