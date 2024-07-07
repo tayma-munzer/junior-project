25048d5adc5301ad3f77348b404574cf04688fa1
Halafadi2004 <halamerei1@gmail.com>
2024-07-07 04:44:14 +0300

    pul of project

--- our-app/mobile/lib/viewaltservice.dart
+++ our-app/mobile/lib/viewaltservice.dart
@@ -1,19 +1,1 @@
-import 'package:flutter/material.dart';
-import 'package:mobile/appbar.dart';
-import 'package:mobile/drawer.dart';
 
-class Viewaltservice extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(33.0),
-        child: CustomAppBar(),
-      ),
-      drawer: CustomDrawer(),
-      body: Center(
-        child: Text('Wallet Page Content'),
-      ),
-    );
-  }
-}
